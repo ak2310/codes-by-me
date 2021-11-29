4ad9fe9cbb3ad8b8d52c49a8b5264e1234a03ceb
ak2310 <91021858+ak2310@users.noreply.github.com>
2021-11-29 11:03:34 +0530

    New code for tower of hanoi added

---
+++ towerofhanoi.py
@@ -0,0 +1,9 @@
+def toh(n,s,d,a):
+    if n==1:
+        print("Move disk",n,"from source",s,"to destination",d)
+        return
+    toh(n-1,s,a,d)
+    print("Move disk",n,"from source",s,"to destination",d)
+    toh(n-1,a,d,s)
+n=3
+toh(n,'A','C','B')
