0775238ed32579ee4a9056a00e8d99bd57d8bd10
ak2310 <91021858+ak2310@users.noreply.github.com>
2021-12-06 21:40:02 +0530

    real code created manually added

--- mergesort.py
+++ mergesort.py
@@ -1,61 +1,43 @@
-# Python program for implementation of MergeSort
-def mergeSort(arr):
-	if len(arr) > 1:
-
-		# Finding the mid of the array
-		mid = len(arr)//2
-
-		# Dividing the array elements
-		L = arr[:mid]
-
-		# into 2 halves
-		R = arr[mid:]
-
-		# Sorting the first half
-		mergeSort(L)
-
-		# Sorting the second half
-		mergeSort(R)
-
-		i = j = k = 0
-
-		# Copy data to temp arrays L[] and R[]
-		while i < len(L) and j < len(R):
-			if L[i] < R[j]:
-				arr[k] = L[i]
-				i += 1
-			else:
-				arr[k] = R[j]
-				j += 1
-			k += 1
-
-		# Checking if any element was left
-		while i < len(L):
-			arr[k] = L[i]
-			i += 1
-			k += 1
-
-		while j < len(R):
-			arr[k] = R[j]
-			j += 1
-			k += 1
-
-# Code to print the list
+from array import array
 
 
-def printList(arr):
-	for i in range(len(arr)):
-		print(arr[i], end=" ")
-	print()
+def ms(array):
+    if len(array)>1:
+        mid=len(array)//2
+        L=array[:mid]
+        R=array[mid:]
+        ms(L)
+        ms(R)
 
+        i=j=k=0
+        while i<len(L) and j<len(R):
+            if L[i]<R[j]:
+                array[k]=L[i]
+                i +=1
+            else:
+                array[k]=R[j]
+                j+=1
+            k+=1
+        
+        while i<len(L):
+            array[k]=L[i]
+            i+=1
+            k+=1
 
-# Driver Code
-if __name__ == '__main__':
-	arr = [12, 11, 13, 5, 6, 7]
-	print("Given array is", end="\n")
-	printList(arr)
-	mergeSort(arr)
-	print("Sorted array is: ", end="\n")
-	printList(arr)
+        while j<len(R):
+            array[k]=R[j]
+            j+=1
+            k+=1
 
+def printlist(array):
+    for i in range(len(array)):
+        print(array[i],end=" ")
+    print()
 
+if __name__=='__main__':
+    array=[12,58,96,3,57]
+    print("Given array is",end="\n")
+    printlist(array)
+    ms(array)
+    print("Sorted array is:",end="\n")
+    printlist(array)
