13d42c2300617d20744193b06710f89b8f080c5d
ak2310 <91021858+ak2310@users.noreply.github.com>
2021-12-21 08:19:11 +0530

    Code added

--- regularexp.py
+++ regularexp.py
@@ -2,3 +2,12 @@ import re
 string="This is python class"
 result=re.findall('python',string)
 print(result)
+
+### re in phone number
+
+string=input()
+pattern='(0|+91)?[1-9][0-9]{9}'
+if re.match(pattern,string):
+    print("Valid")
+else:
+    print("Invalid")
